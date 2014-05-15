278c03b972a7973188e4b4023fa2adf540c5dfb2
cowboy76 <cowboy76@ncsoft.com>
2014-05-15 17:55:10 +0900

    lamda apply

---
+++ Lamda/src/section4/PersonContact.java
@@ -0,0 +1,29 @@
+package section4;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import section2.Person;
+
+public class PersonContact {
+	
+	public void checkOld(List<Person> p1, Predicate<Person> pred) {
+		for (Person p : p1) {
+			if (pred.test(p)) {
+				call(p);
+			}
+		}
+	}
+
+	public void checkYoungMan(List<Person> p1, Predicate<Person> pred) {
+		for (Person p : p1) {
+			if (pred.test(p)) {
+				call(p);
+			}
+		}
+	}
+
+	public void call(Person p) {
+		System.out.println(p.toString());
+	}
+}
---
+++ Lamda/src/section4/PersonTest4.java
@@ -0,0 +1,25 @@
+package section4;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import section2.Person;
+import section2.Person.Gender;
+
+public class PersonTest4 {
+
+	public static void main(String[] args) {
+		List<Person> pl = Person.createShortList();
+		PersonContact personContact = new PersonContact();
+
+		Predicate<Person> isOld = p -> p.getAge() >= 50;
+		Predicate<Person> isYoungMan = p -> p.getAge() >= 20 && p.getAge() <= 29 && p.getGender() == Gender.MALE;
+		
+		System.out.println("50 over");
+		personContact.checkOld(pl, isOld);
+		
+		System.out.println("20 over");
+		personContact.checkYoungMan(pl, isYoungMan);
+	}
+
+}
